f909710fc9e8094bd4daa2b5a9ec589f4bf58445
demtellme <0alexgorbachev0@gmail.com>
2026-03-07 16:56:00 +0000

    Initial commit Program to make images into ascii symbols

---
+++ asciify.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import math
+
+ASCII_CHARS = ["@","#","$","%","?","*","+",";",":",",","."]
+
+imgpath = input("Please provide a path for the image you wanna ascii-ify; ")
+pathparts = imgpath.split("/")
+imgname = pathparts[-1]
+
+nameparts = imgname.split(".")
+imgname = nameparts[1]
+
+
+img = Image.open(imgpath)
+W,H = img.size[0], img.size[1]
+ratio = H/W / 3
+
+print(f"the ratio of height to width is {ratio},")
+newwidth = int(input("How wide do you want the ascii version of the image to be: "))
+
+newheight = math.floor(newwidth * ratio) 
+
+newimg =  img.resize((newwidth,newheight)).convert("L")
+
+pixels = list(newimg.get_flattened_data())
+characters = "".join([ASCII_CHARS[int(pixel)//25] for pixel in pixels])
+
+pixelcount = len(characters)
+
+asciiimage = "\n".join([characters[i:(i+newwidth)] for i in range(0,pixelcount,newwidth)])
+
+with open(f"{imgname}output.txt", "w") as f:
+    f.write(asciiimage)
